05cf8326fd876aa2d47f9e429156b784fafc3b1c
Jonathan Tan <jtsz5057@gmail.com>
2024-03-06 19:00:56 +1000

    create properties page for displaying listing

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,21 @@
-import Link from 'next/link';
+import properties from '@/properties.json'
 
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className="text-exl">Properties</h1>
-      <a href='/'>Go Home</a>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        { properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          { properties.map((property) => (
+            <div>{property.name}</div>
+          )) }
+        </div>
+        ) }
+        
+      </div>
+    </section>
   )
 }
 
