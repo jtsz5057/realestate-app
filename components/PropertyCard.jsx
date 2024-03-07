5d5c9524f4992c52dd37e4feb7c3c0ed8b03320a
Jonathan Tan <jtsz5057@gmail.com>
2024-03-07 23:53:05 +1000

    PropertyCard - add icons and add dynamic field to the listings

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker } from 'react-icons/fa';
 
 const PropertyCard = ({ property }) => {
   const getRateDisplay = () => {
@@ -16,37 +17,37 @@ const PropertyCard = ({ property }) => {
 
   return (
     <div className="rounded-xl shadow-md relative">
-        <Image
-         src="/images/properties/a1.jpg"
-         alt=""
-         height={0}
-         width={0}
-         sizes='100vw'
-         className='w-full h-auto rounded-t-xl'
-        />
-        <div className="p-4">
-            <div className="text-left md:text-center lg:text-left mb-6">
-                <div className="text-gray-600">{property.type}</div>
-                    <h3 className="text-xl font-bold">{property.name}</h3>
-                </div>
-              <h3
+      <Image
+       src="/images/properties/a1.jpg"
+       alt=""
+       height={0}
+       width={0}
+       sizes='100vw'
+       className='w-full h-auto rounded-t-xl'
+       />
+      <div className="p-4">
+        <div className="text-left md:text-center lg:text-left mb-6">
+          <div className="text-gray-600">{property.type}</div>
+            <h3 className="text-xl font-bold">{property.name}</h3>
+             </div>
+               <h3 
                 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"
-              >
+               >
                 ${getRateDisplay()}
               </h3>
 
               <div className="flex justify-center gap-4 text-gray-500 mb-4">
                 <p>
-                  <i className="fa-solid fa-bed"></i> 3
+                  <FaBed className='inline mr-2' />{ property.beds }
                   <span className="md:hidden lg:inline">Beds</span>
                 </p>
                 <p>
-                  <i className="fa-solid fa-bath"></i> 2
+                  <FaBath className='inline mr-2' />{ property.baths }
                   <span className="md:hidden lg:inline">Baths</span>
                 </p>
                 <p>
-                  <i className="fa-solid fa-ruler-combined"></i>
-                  1,500 <span className="md:hidden lg:inline">sqft</span>
+                  <FaRulerCombined className='inline mr-2' />{property.square_feet}
+                  <span className="md:hidden lg:inline">sqft</span>
                 </p>
               </div>
 
@@ -61,17 +62,15 @@ const PropertyCard = ({ property }) => {
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-                  <i
-                    className="fa-solid fa-location-dot text-lg text-orange-700"
-                  ></i>
-                  <span className="text-orange-700"> Boston MA </span>
+                  <FaMapMarker className='text-orange-700 mt-1'/>
+                  <span className="text-orange-700"> {' '}{property.location.city} {property.location.state}{' '} </span>
                 </div>
-                <a
-                  href="property.html"
+                <Link
+                  href={`/properties/${property._id}`}
                   className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                   Details
-                </a>
+                </Link>
               </div>
             </div>
           </div>
