1cd2bcecfe91a89e04ad3916bb7bb7cdd8c0972c
Jonathan Tan <jtsz5057@gmail.com>
2024-03-16 23:04:19 +1000

    create fetching single property data from api to client side

--- utils/requests.js
+++ utils/requests.js
@@ -20,4 +20,25 @@ async function fetchProperties() {
     }
   }
 
-  export { fetchProperties };
+  // Fetch single properties 
+  async function fetchProperty(id) {
+    try {
+      // Handle the case where the domain is not available yet
+      if (!apiDomain) {
+        return null;
+      }
+
+      const res = await fetch(`${apiDomain}/properties/${id}`);
+  
+      if (!res.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      return res.json();
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  export { fetchProperties, fetchProperty };
