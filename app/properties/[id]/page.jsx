a0f1e880e14224bd17b110b25cc89ed462a422da
Jonathan Tan <jtsz5057@gmail.com>
2024-03-17 22:27:23 +1000

    create PropertyHeaderImage component for property page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -27,10 +28,20 @@ const PropertyPage = () => {
     }
    }, [id, property]);
 
+   if (!property && !loading) {
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    )
+   }
+
   return (
-    <div>
-      PropertyPage
-    </div>
+    <>
+      {!loading && property && (<>
+        <PropertyHeaderImage image={ property.image[0]}/>
+      </>)}
+    </>
   )
 }
 
