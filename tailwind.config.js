34a3402f20a150c43ba64362d9a5987d9186196a
Jonathan Tan <jtsz5057@gmail.com>
2024-03-23 23:42:18 +1000

    add custom class for grid template on property page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['Poppi', 'sans-serif'],
       },
+
+      // custom class to split 70/30 of the grid template on property details page
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      }
     },
   },
   plugins: [],
